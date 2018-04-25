bd49563bf6ea4d40b22b6261f61ded8bbb3ce180
Mark Theiding <mark.theiding@gmail.com>
2018-04-24 21:54:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,229 @@
+# Look Alexa, no Remote!
+[Watch the skill in action (Coming soon)](XXX)
+
+Eliminate the need for any physical remote controls and instead control all of your entertainment devices entirely through Alexa using voice commands.
+
+Remote controls are frustrating	- they are never where you need them. With Alexa the opportunity has finally arrived to get rid of those infrared foes. But unfortunately there is no existing Alexa skill that can truly replace remote controls. Sure, via Alexa's Smart Home skills you can turn your TV on and off, control volume, issue playback command and invoke predefined activities - no problem. But how do you complete more complex tasks like watching your latest recording, pick a movie on your Apple TV or change your TV settings? 
+
+This project aims to fill the gap. Plus it hopefully serves as a helpful tutorial in general on how to create your own non-trivial Alexa skill. 
+
+You will [run the Alexa skill in AWS](#1-alexa-skill-setup) and [provide a web service to send remote control commands](#2-remote-control-web-service-setup) on a server of your choice (based on [openHAB](http://www.openhab.org) and [Harmony Hub](https://www.logitech.com/en-us/product/harmony-hub) if you leverage the [sample web service implementation](#2-2-sample-rest-api-implementation-with-openhab)). 
+
+
+# 1. Alexa Skill Setup
+[Watch the Alexa skill setup process (Coming soon)](XXX)
+
+## 1.1. Prerequisites
+1. [Setup of ASK CLI](https://developer.amazon.com/docs/smapi/quick-start-alexa-skills-kit-command-line-interface.html) (version 1.1.2 or newer)
+2. [Install npm](https://nodejs.org/en/download/)
+
+## 1.2. Installation
+1. Download the repository
+	```
+	$ git clone https://github.com/theiding/look-alexa-no-remote
+	```
+	If you don't want to use git, go to <https://github.com/theiding/look-alexa-no-remote>, click on "Clone or download", "Download ZIP" and unzip the source code.
+    
+2. Install Node.js dependencies for AWS Lambda function
+	```
+	$ cd look-alexa-no-remote/alexa/remote-control/lambda/custom
+	$ npm install
+	```
+3. Create your own configuration file from the sample file
+ 	```bash
+	$ cp config.js.sample config.js
+	```
+4. Create and deploy skill and lambda function
+	```
+	$ cd ../..
+	$ ask deploy
+	```
+
+## 1.3. Testing
+You can now test your skill. Of course there won't be any remote control command execution yet since we haven't yet [setup the remote control web service](#2-remote-control-web-service-setup), but we can start interacting with Alexa and review logging output from our Lambda function. 
+1. Use the skill
+	
+    Tell Alexa the following and verify Alexa's responses
+	```
+	"Alexa, remote control" -> "Ok, ready."
+	"Right" -> "Ok"
+	"Switch to Apple TV" -> "Ok"
+	"Done" -> "Ok. Done."
+	```
+2. Review Lambda logs
+	
+	[Go to the logs for your Lambda function](https://docs.aws.amazon.com/lambda/latest/dg/monitoring-functions-logs.html).
+	Verify that each button command was logged correctly by looking for the respective `sendCmd` log entries:
+
+	- `sendCmd: itemCmd['item']=EntertainmentBedroomTV,['cmd']=Mute` This "Mute" command is sent to your TV when the session is started. This is to avoid any interference from the TV sound with subsequent commands.
+    - `sendCmd: itemCmd['item']=EntertainmentBedroomDVR,['cmd']=DirectionRight` The "Right" button is sent in direct response to the "Right" voice command.
+	- `itemCmd['item']=EntertainmentBedroomTV,['cmd']=InputHDMI2` Switching to Apple TV is executed by changing the input on the TV via the input command.
+	- `sendCmd: itemCmd['item']=EntertainmentBedroomTV,['cmd']=Mute` We unmute the TV (by toggling "Mute") when our session ends.
+	
+## 1.4. Configuration
+Now let's configure the skill to handle your own entertainment devices.
+1. Voice model
+
+	The voice model leverages a list of buttons and devices to define what set of buttons and devices are allowed in skill invocations. Configure the list of buttons and devices by editing `look-alexa-no-remote/model/en-US.json`:
+	- List of buttons
+
+		Find the `LIST_OF_REMOTE_BUTTONS`. This list defines all the remote buttons that are suppported for your devices.
+        
+		Each entry contains an `id` that uniquely identifies a button in a device independent fashion (e.g. `DirectionUp` identifies the "Up" button, wether it's for an Apple TV, a DVR or some other device).
+		The `value` entry specifies how the user can refer to the button. If you want to allow multiple names use the `synonynm` entry (e.g. in the sample config file the user can call the "Ok" button `ok`, `enter` or `select`).
+        
+		Update the list by removing buttons you don't need or by adding ones that are missing for your setup.
+	- <a id="list-of-devices"></a>List of devices
+
+		Find the `LIST_OF_TV_SOURCES`. This list defines all the devices that are suppported.
+        
+		Each entry contains an `id` that uniquely identifies the device. The device id will be sent in the [REST API call for each button command](#2-1-rest-api).
+        
+		The `value` entry specifies how the user can refer to the device. If you want to allow multiple names use the `synonynm` entry (e.g. in the sample config file the user can call the TV `TV` or `television`).
+        
+		Update the list by removing devices you don't need or by adding ones that are missing for your setup. <br>
+2. Skill and devices configuration
+
+	Provide general skill and device configuration by editing `alexa-remote-control/lambda/custom/config.js`:
+	- <a id="skill-configuration"></a>Skill - Configure the general skill settings in the `skill` object. Follow the comments next to each property. 
+	- <a id="skill-configuration"></a>Devices - For each device that you defined in the [list of devices](#list-of-devices)  above specify a section in the `device` object with the name of the device id. Within each device id object provide:
+		- <a id="button-command-configuration"></a>`buttons` - The buttons map maps the button id to a device and web service specific button command. The device specific button command is sent in the REST API call (not the generic button id). Note that in most cases button id and button command are the same, but at times they differ (e.g. a Harmony Hub based web service will expect button command `OK` for the "OK" button for an Xfinity DVR, but `Select` for the "OK" button for an Apple TV).
+		
+			If a button doesn't have a map entry then that means it is not supported by the device.<br>
+		- `input` - Specify the TV's button command needed to change the TV's input to the given device.
+		- `name` - Specify a user friendly name for the device. The skill uses this value for user friendly error messages.
+	
+    	Remove the device id objects that you don't need (e.g. `EntertainmentBedroomDVR`).
+        
+3. Re-deploy your configuration changes
+	```
+    $ cd look-alexa-no-remote/alexa/remote-control
+	$ ask deploy
+	```
+
+# 2. Remote Control Web Service Setup
+The skill is calling a web service for each requested button press. The web service is responsible for sending the actual button command to the various devices.
+
+## 2.1 REST API
+Each button command is sent using the following REST API invocation:
+```
+{serviceHost}:{servicePort}/rest/items/{item}
+```
+with body
+```
+{cmd}
+```
+where
+- `{serviceHost}` is the host configured in the [skill configuration](#skill-configuration)
+- `{sericePort}` is the port configured in the [skill configuration](#skill-configuration)
+- `{item}` is the name of the device receiving the command as configured in the [devices configuration](#devices-configuration)  (e.g. `EntertainmentBedroomDVR` for the sample config file)
+- `{cmd}` is the name of the button command for the device as configured in the [button command configuration](#button-command-configuration)(e.g. `Select` for `EntertainmentBedroomAppleTV` from the sample config file)
+
+For example with the sample files a request to press the "OK" button on the Apple TV would be sent to 
+```
+111.111.111.111:8080/rest/items/EntertainmentBedroomAppleTV
+```
+with body
+```
+Select
+```
+You can implement this REST API in whatever fashion best matches your existing system setup or you can use the provided sample implementation below.
+
+## 2.2 Sample REST API Implementation with openHAB
+This implementation of the above [REST API](#2-1-rest-api) leverages [openHAB](http://www.openhab.org) and [Harmony Hub](https://www.logitech.com/en-us/product/harmony-hub). 
+
+### 2.2.1. Prerequisites
+1. [Install openHAB](https://docs.openhab.org/installation/index.html)(version 2.1 or newer)
+2. [Install Harmony Hub](https://support.myharmony.com/en-au/hub)
+
+### 2.2.2 openHAB Configuration
+1. Create your own `.things` and `.items` file from the sample files
+	```
+	$ cd look-alexa-no-remote/openhab
+	$ cp remote-control.things.sample remote-control.things
+	$ cp remote-control.items.sample remote-control.items
+	```
+2. <a id="thing"></a>Edit "thing"
+	
+    Edit `look-alexa-no-remote/openhab/remote-control.things`. This file [integrates your Harmony Hub with openHAB](https://docs.openhab.org/addons/bindings/harmonyhub/readme.html) by defining a "thing" that represents the Harmony Hub.
+    - Replace the host IP with the IP for your Harmony Hub (make sure it has a static IP that won't change with reboots).
+    - For each of your entertainment devices enter one line of the following format:
+    	```
+		device {deviceThingName} [name="{deviceHarmonyHubName}"]
+    	```
+		where
+		- `{deviceThingName}` is the name you define for your device in openHAB
+		- `{deviceHarmonyHubName}` is the name of the device as configured in the Harmony Hub
+	
+    	Example: `device bedroomTV [name="Bedroom TV"]`
+
+3. Edit "items"
+
+	Edit `look-alexa-no-remote/openhab/remote-control.items`. This file defines an openHAB item for each device and [exposes the respective remote control web service](https://docs.openhab.org/configuration/restdocs.html).
+    
+    For each of your entertainment devices enter one line of the following format:
+	```
+	String {item} { channel="harmonyhub:device:main:{deviceThingName}:buttonPress" }
+	```
+	where
+	- `{item}` is the name of the device configured in the [list of devices](#list-of-devices)
+	- `{deviceThingName}`is the device thing name you defined [above](#thing).
+	
+    Example: `String EntertainmentBedroomTV { channel="harmonyhub:device:main:bedroomTV:buttonPress" }`
+4. Copy your configuration files to your openHAB server. 
+
+	[File destination location](https://docs.openhab.org/installation/linux.html#file-locations) depends on your openHAB install. For a repository installation:
+	``` 
+	cd look-alexa-no-remote/openhab
+	cp remote-control.things /etc/openhab2/things
+	cp remote-control.items /etc/openhab2/items
+	``` 
+5. Start openHAB Server	
+
+	[Start your openHAB server depending on your installation method](https://docs.openhab.org/installation/index.html) so it starts answering REST API calls.
+    
+6. Update host and port configuration
+
+	In [the skill configuration](#skill-configuration) update
+	- `serviceHost` to the IP of the device running your openHAB server. Make sure the IP is publicly visible so your Lambda function can reach it while [considering security implications](https://docs.openhab.org/installation/security.html). 
+	- `servicePort` to `8080`
+
+# 2.2.3 Testing
+1. Trigger a manual REST API call from the command line:
+	```
+	curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "{cmd}" 	"http://{serviceHost}:8080/rest/items/{item}"
+	```
+	where
+	- `{cmd}` is the button command you want to test
+	- `{serviceHost}` is the IP of the device running your openHAB server
+	- `{item}` is the openHAB item for the device you want to test
+	    
+	Example:
+	```
+	curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d 	"DirectionRight" "http://111.111.111.111:8080/rest/items/EntertainmentBedroomDVR"
+	```
+	Your test command should now execute on your test device. 
+2. End to End Test
+	
+    Execute remote control commands via Alexa and throw away your remote controls.
+# 3. Troubleshooting FAQ
+1. How do I troubleshoot my Alexa skill?
+
+	Always start by [reviewing your Lambda function logs](https://docs.aws.amazon.com/lambda/latest/dg/monitoring-functions-logs.html). 
+2. How can I test my Alexa skill without having implemented the remote control web service?
+
+	Simply [configure `serviceHost`](skill-configuration) as `111.111.111.111`. This will supress the actual REST API call. 
+3. Yikes, my `en-US.json` file is messed up somehow, but I don't know where. Help!
+
+	Validate your json file with <https://jsonlint.com>.
+4. That config.js file is gnarly to edit. How do I know I got it right before I deploy?
+
+	Validate your .js file with <http://esprima.org/demo/validate.html>.
+4. My openHAB implementation of the REST API doesn't work. What to do?
+	
+    Start by [reviewing openHAB log files](https://docs.openhab.org/administration/logging.html).
+	- `events.log` logs all commands sent to your items over the openHAB message bus via the REST API call, e.g. 
+		```
+    	[ItemCommandEvent] - Item 'EntertainmentBedroomDVR' received command DirectionRight
+        ```
+	- `openhab.log` logs miscellaneous operational information. In particular make sure there are no errors re. your items, things and Harmony Hub binding.
---
+++ alexa/remote-control/lambda/custom/index.js
@@ -0,0 +1,312 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+var Http = require('http');
+var Config = require('./config');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(	
+		generalHandlers, 
+		remoteButtonHandlers
+	);
+    alexa.execute();
+};
+
+const states = {
+	PRESS_REMOTE_BUTTON_MODE: '_pressRemoteButtonMODE'	
+};
+
+// Alexa prompt/response strings
+const ALEXA_HELP = "I can be your remote control for your tv and attached devices so you don't have to use your physical remotes. To start just say the name of a button you want me to press. For example you can say Menu, Left or Select. To switch to a different device use the Switch to command. For example say Switch to Apple TV. Which button would you like me to press?";
+const ALEXA_REQUEST_BUTTON = "Please say a button that you would like me to press.";
+const ALEXA_OK = "Ok";
+const ALEXA_GENERAL_ERROR = "Sorry, something went wrong.";
+const ALEXA_EMPTY_REPROMPT = "";	// Note that we are re-prompting after a successful command with an empty string. 
+									// That effectively gives the user twice the time to say the next key without getting interrupted by a reprompt. 
+									// What we really would like is of course for Amazon to allow us to configure the time until reprompt. 
+
+var generalHandlers = {
+	'AMAZON.StopIntent': abort,
+	'AMAZON.CancelIntent': abort,
+    'AMAZON.HelpIntent': function() {
+		var cmdPromise = startSession(this);
+		emit(this, ALEXA_HELP, ALEXA_REQUEST_BUTTON, cmdPromise);
+    },
+    'Unhandled': function() {	// We wan't our session to be started with the shortest possible voice command - and that is just the skill name (i.e. "Alexa, remote control").
+								// Since "Alexa, remote control" doesn't invoke any intent it triggers the 'Unhandled' handler. We use this to start our session.
+		var cmdPromise = startSession(this);
+		emit(this, "Ok. Ready.", ALEXA_REQUEST_BUTTON, cmdPromise);
+	}
+};
+
+var remoteButtonHandlers = Alexa.CreateStateHandler(states.PRESS_REMOTE_BUTTON_MODE, {
+    'PressRemoteButtonIntent': function() {
+		var buttonCmds = [];
+		buttonCmds.push({'remoteButtonSlot': this.event.request.intent.slots.RemoteButton, 'pressCountSlot': this.event.request.intent.slots.PressCount});
+		pressRemoteButtons(this, buttonCmds);
+	},
+    'PressTwoRemoteButtonsIntent': function() {
+		var buttonCmds = [];
+		buttonCmds.push({'remoteButtonSlot': this.event.request.intent.slots.RemoteButtonOne, 'pressCountSlot': this.event.request.intent.slots.PressCountOne});
+		buttonCmds.push({'remoteButtonSlot': this.event.request.intent.slots.RemoteButtonTwo, 'pressCountSlot': this.event.request.intent.slots.PressCountTwo});
+		pressRemoteButtons(this, buttonCmds);
+	},
+	'SwitchTVSourceIntent': function() {
+		switchTVSource(this, this.event.request.intent.slots.TVSource);
+	},
+	'AMAZON.StopIntent': abort,
+	'AMAZON.CancelIntent': abort,
+	'AMAZON.HelpIntent': function() {
+		emit(this, ALEXA_HELP, ALEXA_REQUEST_BUTTON);
+	},
+	'SessionEndedRequest': function() {
+		console.log('SessionEndedRequest received');
+		endSession();
+	},
+ 	'Unhandled': function() {
+		emit(this, "Sorry, I didn't understand that. Please try again.", ALEXA_REQUEST_BUTTON);
+	}
+});
+
+function startSession(alexa) {
+	console.log("startSession");
+	
+	alexa.handler.state = states.PRESS_REMOTE_BUTTON_MODE;
+	alexa.attributes['TVSOURCE'] = Config.skill.defaultDevice; // We start out by sending button presses to the default device
+	alexa.attributes['WINDING'] = false; // We are not winding until user presses fast forward or rewind
+	return sendCmd({'item': Config.skill.tv, 'cmd': Config.skill.tvMute}); // Mute the tv so it won't interfere with subsequent voice commands
+}
+
+function endSession() {
+	console.log("endSession");
+
+	return sendCmd({'item': Config.skill.tv, 'cmd': Config.skill.tvUnmute}); // Unmute the tv when we are done
+}
+
+// Press multiple buttons as defined by the buttonCmds array.
+// Each buttonCmd must provide property remoteButtonSlot to specify the button as well as property pressCountSlot for repeated button presses. 
+function pressRemoteButtons(alexa, buttonCmds) {
+	var responseStr = null;
+	var repeatStr = null;
+	var cmdPromise = null;
+	var result = {'cmdPromise': null, 'done': false};
+	
+	console.log("pressRemoteButtons");
+	
+	try {
+		for(var i = 0; i < buttonCmds.length; i++) {
+			if (!result.done)
+				result = pressRemoteButton(alexa, buttonCmds[i].remoteButtonSlot, buttonCmds[i].pressCountSlot, result.cmdPromise);
+
+			responseStr = result.done ? "Ok. Done." : ALEXA_OK;
+			repeatStr = result.done ? null : ALEXA_EMPTY_REPROMPT;
+		}
+	}
+	catch (err) {
+		console.log("pressRemoteButtons err=");
+		console.log(err); // Need to log err separately to properly log both Strings and objects :-(
+		responseStr = err.responseStr;
+		repeatStr = err.repeatStr;
+	}
+
+	emit(alexa, responseStr, repeatStr, result.cmdPromise);
+}	
+
+function pressRemoteButton(alexa, remoteButtonSlot, pressCountSlot, cmdPromise) { 
+	var result = {'cmdPromise': cmdPromise, 'done': false};
+	var pressCount = (typeof pressCountSlot.value == 'undefined' || pressCountSlot.value == null) ? 1 : pressCountSlot.value;
+	
+	console.log("pressRemoteButton: remoteButton=" + remoteButtonSlot.value + " pressCount=" + pressCount);
+
+	if (remoteButtonSlot.value == null || typeof remoteButtonSlot.value == 'undefined')
+		throw {
+			'responseStr': "Sorry, I didn't understand your request. Please try again.",
+			'repeatStr': ALEXA_REQUEST_BUTTON
+		};	
+	var remoteButtonResult = slot(remoteButtonSlot);
+	if (remoteButtonResult) { // We were able to match the button
+		if (remoteButtonResult.id == 'Done') { // User requested to end the session
+			result.cmdPromise = endSession();
+			result.done = true;
+		}
+		else {
+			var deviceCmdForButton = getDeviceCmdForButton(alexa.attributes['TVSOURCE'], remoteButtonResult.id); // Get device command for device that is current TV source
+			if (typeof deviceCmdForButton == 'undefined') { // Requested button is not supported by current device
+				console.log("Button " + remoteButtonResult.value + " not supported by " + alexa.attributes['TVSOURCE']);
+				throw {
+					'responseStr': "Sorry, the " + remoteButtonResult.value + " button is not supported by your " + Config.device[alexa.attributes['TVSOURCE']]['name'] + ". Please try again.",
+					'repeatStr': ALEXA_REQUEST_BUTTON
+				};
+			}
+			else { // valid button
+				// Check if we need to inject a play command to prevent "runaway winding" (user is actively winding, but then pressed a non-winding key, which was probably a mistake and leads to continued winding)
+				var wasWinding = alexa.attributes['WINDING'];
+				// Update whether we are currently winding
+				alexa.attributes['WINDING'] = (remoteButtonResult.id == 'FastForward') || (remoteButtonResult.id == 'Rewind');
+				// If we were actively winding and now no longer wind and button is not stop/pause/play then we inject a play command to prevent "runaway winding"
+				if (wasWinding && !alexa.attributes['WINDING'] && ((remoteButtonResult.id != 'Play') && (remoteButtonResult.id != 'Pause') && (remoteButtonResult.id != 'Stop'))) {
+					var playCmd = getDeviceCmdForButton(alexa.attributes['TVSOURCE'], 'Play');
+					if (typeof playCmd != 'undefined') // Make sure play command is supported for current device
+						result.cmdPromise = sendCmd({'item': alexa.attributes['TVSOURCE'], 'cmd': playCmd}, result.cmdPromise);
+				}
+				
+				// Repeat our itemCmd "pressCount" times
+				var itemCmd = {'item': alexa.attributes['TVSOURCE'], 'cmd': deviceCmdForButton};
+				for(var i = 0; i < pressCount; i++)
+					result.cmdPromise = sendCmd(itemCmd, result.cmdPromise);
+			}
+		}
+	}
+	else { // Can't serve the request because remoteButtonResult can't be matched
+		throw {
+			'responseStr': "Sorry, the " + remoteButtonSlot.value + " button is not supported by your " + Config.device[alexa.attributes['TVSOURCE']]['name'] + ". Please try again.",
+			'repeatStr': ALEXA_REQUEST_BUTTON
+		};
+	}
+	return result;
+}
+
+// Convert button id to device specific button command
+function getDeviceCmdForButton(device, remoteButtonId) {
+	if (typeof Config.device == 'undefined')
+		throw "Config is missing device property";
+	if (typeof Config.device[device] == 'undefined')
+		throw "Config.device is missing " + device + " property";
+	if (typeof Config.device[device]['buttons'] == 'undefined')
+		throw "Config.device." + device + " is missing buttons property";
+
+	var deviceCmdForButton = Config.device[device]['buttons'][remoteButtonId];
+	console.log("getDeviceCmdForButton: deviceCmdForButton=" + deviceCmdForButton);
+	return deviceCmdForButton;
+}
+
+// Switch TV input to new device tvSourceSlot. Future button presses will be sent to this new device.
+function switchTVSource(alexa, tvSourceSlot) {
+	console.log("switchTVSource");
+
+	var currentTVSource = alexa.attributes['TVSOURCE'];
+	var tvSourceResult = slot(tvSourceSlot); 
+	if (tvSourceResult) {
+		alexa.attributes['TVSOURCE'] = tvSourceResult.id; // Maintain new source item in session so that future button presses are sent to this new device
+		console.log("attributes['TVSOURCE']=" + alexa.attributes['TVSOURCE']);
+		alexa.attributes['WINDING'] = false; // Reset since we have a new source
+		
+		var cmdPromise = null;
+		if (currentTVSource == Config.skill.tv)
+			cmdPromise = sendCmd({'item': Config.skill.tv, 'cmd': 'Smart'}, cmdPromise);
+		cmdPromise = sendCmd({'item': Config.skill.tv, 'cmd': Config.device[tvSourceResult.id]['input']}, cmdPromise);
+		emit(alexa, ALEXA_OK, ALEXA_EMPTY_REPROMPT, cmdPromise);
+	}
+	else {
+		// Can't serve the request because TVSource can't be matched
+		console.log("Can't switch to " + alexa.event.request.intent.slots.TVSource.value);
+		var response = "Sorry, I don't know how to switch to the " + alexa.event.request.intent.slots.TVSource.value + ". Please try again.";
+		emit(alexa, response, ALEXA_REQUEST_BUTTON);
+	}
+}
+
+// Collect details about whether/how slot was matched
+function slot(slot) {
+	var result = null;
+	if (slot.resolutions && slot.resolutions.resolutionsPerAuthority && slot.resolutions.resolutionsPerAuthority.length > 0 && slot.resolutions.resolutionsPerAuthority[0] &&
+		slot.resolutions.resolutionsPerAuthority[0].status.code == 'ER_SUCCESS_MATCH') {
+		// User response for slot was matched to canonical value or synonym
+		result = {};
+		result.value = slot.value; // Actual string that was matched with user response
+		result.id = slot.resolutions.resolutionsPerAuthority[0].values[0].value.id; // id for matched slot value
+		result.canonicalValue = slot.resolutions.resolutionsPerAuthority[0].values[0].value.name; // Canonical value for matched slot value
+		console.log("slot: slot=" + slot.name + ",result.value=" + result.value + ",result.id=" + result.id + ",result.canonicalValue=" + result.canonicalValue);
+	}
+	else
+		console.log("slot: Couldn't slot " + slot.value);	
+		
+	return result;
+}
+
+// Emit Alexa's response. If repeatStr is not specified we respond in tell mode. Otherwise we respond in ask mode. 
+// If cmdPromise is specified for pending button commands we chain our emit to guarantee that emit is called after all button commands completed. 
+function emit(alexa, responseStr, repeatStr, cmdPromise)
+{
+	if (typeof responseStr == 'undefined' || responseStr == null)
+		responseStr = ALEXA_GENERAL_ERROR;
+	
+	var mode = (typeof repeatStr == 'undefined' || repeatStr == null) ? ':tell' : ':ask';
+		
+	if (typeof cmdPromise != 'undefined' && cmdPromise != null)
+		cmdPromise.then(function(result) {	
+			console.log("emit responseStr=" + responseStr + ",repeatStr=" + repeatStr);
+			alexa.emit(mode, responseStr, repeatStr);
+		}, function(err) {
+			alexa.emit(':tell', ALEXA_GENERAL_ERROR);
+		});		
+	else {
+		console.log("emit responseStr=" + responseStr + ",repeatStr=" + repeatStr);
+		alexa.emit(mode, responseStr, repeatStr);
+	}
+}
+
+// Send button command asynchronously via web service.
+// If cmdPromise is specified we chain our web service execution behind cmdPromise. This ensures ordered execution of button commands.
+// We return the promise for our web service execution for additional future chaining. 
+function sendCmd(itemCmd, cmdPromise) {
+	if (itemCmd == null)
+	{
+		var err = new Error("itemCmd mustn't be null");
+		console.log("sendCmd: " + err);
+		throw err;
+	}
+
+	console.log("sendCmd: itemCmd['item']=" + itemCmd['item'] + ",['cmd']=" + itemCmd['cmd']);
+	if (itemCmd['item'] == null)
+		console.log("Warning: Sending to null item will cause error");
+
+	if (Config.skill.serviceHost != '111.111.111.111') // Skip actual web service call for dummy IP for testing purposes
+		if (cmdPromise == null)
+			cmdPromise = callWebService(itemCmd, Config.skill.serviceHost, Config.skill.servicePort); // First call goes out straight away
+		else
+			cmdPromise = cmdPromise.then(callWebService.bind(null, itemCmd, Config.skill.serviceHost, Config.skill.servicePort)); // Subsequent calls are chained
+
+	return cmdPromise;
+}
+
+// Actual web service invocation
+function callWebService(itemCmd, serviceHost, servicePort) {
+	return new Promise(function(resolve, reject) {
+		console.log("callWebService: start");
+		const options = {
+			host: serviceHost,
+			port: servicePort,
+			path: '/rest/items/' + itemCmd['item'],
+			method: 'POST',
+			headers: {
+				'Content-Type': 'text/plain',
+				'Accept': 'application/json'
+			}
+		};
+		const request = Http.request(options, function(response) {
+			if (response.statusCode !== 200) {
+				response.resume(); // Consume response data to free up memory
+				var err = new Error("Invalid status code: " + response.statusCode);
+				console.log("err=" + err);
+				reject(err);
+				return;
+			}
+			response.on('data', function (chunk) {
+			});
+			response.on('end', function() {	// Successful request completion
+				console.log("callWebService: end");
+				resolve('ok');
+			});
+		});
+		request.on('error', (e) => {			// Couldn't connect
+			console.log("Couldn't connect, " + e);
+			reject(e);
+		});
+		request.write(itemCmd['cmd']);	// Post the command
+		request.end();
+	});
+}
+
+function abort() {
+	emit(this, "Ok. Remote control cancelled.");	
+}
